23c7873464b2d66f86aa6a8c4fe40ce50904d0f1
Guiando <Guiando@G036>
2022-05-24 17:46:25 -0300

    Add project files.

---
+++ QuebraLinha/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace QuebraLinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var nome = "Gilson \n";
+            var sobrenome = "Antunes \n";
+            var sobrenome2 = "Ravaiani \n";
+           
+            Console.WriteLine(nome+sobrenome+sobrenome2);
+            Console.Write(nome+ sobrenome+ sobrenome2);
+            Console.ReadLine();
+        }
+    }
+}
